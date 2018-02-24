2133a2e0b66c8ce404008f2880d387a15943c666
Grayson-dev <mail@grayson.ru>
2018-02-24 15:38:11 +0500

    Calculate with operations

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,41 @@
 public class Calculate {
     public static void main(String[] args) {
+        double result;
+        boolean error = false;
+        
         System.out.println("Calculate...");
+        
+        double a = Double.valueOf(args[0]);
+        double b = Double.valueOf(args[2]);
+        switch (args[1]) {
+            case "+":
+                result = a + b;
+                break;
+            
+            case "-":
+                result = a - b;
+                break;
+            
+            case "*":
+                result = a * b;
+                break;
+            
+            case "/":
+                result = a / b;
+                break;
+            
+            case "^":
+                result = Math.pow(a, b);
+                break;
+            
+            default:
+                result = 0.0;
+                error = true;
+        }
+        
+        if (error)
+            System.out.println("Unknown operation");
+        else
+            System.out.println("Result: " + result);
     }
 }
